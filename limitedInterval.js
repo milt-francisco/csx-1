7a22cf4a836712c245786cfe770795a338deed79
milt-francisco <121690557+milt-francisco@users.noreply.github.com>
2023-12-16 00:57:11 -0500

    Update limitedInterval.js
    fixed to meet intent of the challenge, your repo was the only one I found with any solutions

--- limitedInterval.js
+++ limitedInterval.js
@@ -1,14 +1,6 @@
-const limitedInterval = (cb, wait, limit) => {
-  for (let i = 0; i < limit - wait; i += wait) {setTimeout(cb, wait)};
-};
-
-
+const limitedInterval = (cb, wait, limit) => { //no idea if this is actually correct
+	let interval = setInterval(cb, wait) 	
+	setTimeout(() => {clearInterval(interval)}, limit);
+}
 // /* Uncomment the following line and click 'run' to test your work */
 limitedInterval(() => console.log('repeating'), 100, 550); // should log 'repeating' once per 100 ms, five times.
-
-/* Write a function called limitedInterval that accepts as arguments in this order -
-
-callback function
-wait time in milliseconds
-limit time in milliseconds.
-limitedInterval should run the callback once every wait milliseconds, up to limit milliseconds, and then stop. */
